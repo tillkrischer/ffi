00618b8fed91b8322ff24aabe9363bcbc8f9a860
Till Krischer <till@krischer.io>
2024-09-02 21:13:09 +0200

    click

--- ffi.ts
+++ ffi.ts
@@ -1,6 +1,11 @@
 import { dlopen, FFIType, ptr, toArrayBuffer } from "bun:ffi";
 
 const CF_TEXT = 1;
+const MOUSEEVENTF_LEFTDOWN = 0x02;
+const MOUSEEVENTF_LEFTUP = 0x04;
+const MOUSEEVENTF_RIGHTDOWN = 0x08;
+const MOUSEEVENTF_RIGHTUP = 0x10;
+const MOUSEEVENTF_ABSOLUTE = 0x8000;
 
 const user32 = dlopen("user32.dll", {
   GetForegroundWindow: {
@@ -26,6 +31,19 @@ const user32 = dlopen("user32.dll", {
   GetCursorPos: {
     args: [FFIType.ptr],
     returns: FFIType.int8_t,
+  },
+  SetCursorPos: {
+    args: [FFIType.int32_t, FFIType.int32_t],
+    returns: FFIType.int8_t,
+  },
+  mouse_event: {
+    args: [
+      FFIType.int32_t,
+      FFIType.int32_t,
+      FFIType.int32_t,
+      FFIType.int32_t,
+      FFIType.ptr,
+    ],
   },
 });
 
@@ -61,16 +79,29 @@ function getCursorPos() {
   }
   return {
     x: buffer[0],
-    y: buffer[1]
+    y: buffer[1],
+  };
+}
+
+function setCursorPos(x: number, y: number) {
+  if (user32.symbols.SetCursorPos(x, y) === 0) {
+    throw new Error("cant set cursor pos");
   }
 }
 
-// const a = getClipboard();
-// const b = getWindow()
-//
-// console.log(a);
-// console.log(b);
+function mouseClick(button: "left" | "right") {
+  let flags = 0;
+  if (button === "left") {
+    flags |= MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP;
+  }
+  if (button === "right") {
+    flags |= MOUSEEVENTF_RIGHTDOWN | MOUSEEVENTF_RIGHTUP;
+  }
+
+  user32.symbols.mouse_event(flags, 0, 0, 0, 0)
+}
+
+setCursorPos(3000, 500)
+mouseClick("right")
 
 
-const c = getCursorPos();
-console.log(c)
