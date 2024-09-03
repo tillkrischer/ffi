ad2b8a81b206c9ab1b5a4c8d64005efc0ca3a3b0
Till Krischer <till@krischer.io>
2024-09-03 20:20:37 +0200

    poc

--- ffi.ts
+++ ffi.ts
@@ -1,4 +1,4 @@
-import { dlopen, FFIType, ptr, toArrayBuffer } from "bun:ffi";
+import { dlopen, FFIType, ptr, toArrayBuffer, CString } from "bun:ffi";
 
 const CF_TEXT = 1;
 const MOUSEEVENTF_LEFTDOWN = 0x02;
@@ -6,6 +6,12 @@ const MOUSEEVENTF_LEFTUP = 0x04;
 const MOUSEEVENTF_RIGHTDOWN = 0x08;
 const MOUSEEVENTF_RIGHTUP = 0x10;
 const MOUSEEVENTF_ABSOLUTE = 0x8000;
+const INPUT_KEYBOARD = 1;
+const KEYEVENTF_EXTENDEDKEY = 0x0001;
+const KEYEVENTF_KEYUP = 0x0002;
+const VK_CONTROL = 0x11;
+const VK_MENU = 0x12;
+const C_KEY = 0x43;
 
 const user32 = dlopen("user32.dll", {
   GetForegroundWindow: {
@@ -45,6 +51,13 @@ const user32 = dlopen("user32.dll", {
       FFIType.ptr,
     ],
   },
+  SendInput: {
+    args: [FFIType.int32_t, FFIType.ptr, FFIType.int32_t],
+    returns: FFIType.int32_t,
+  },
+  keybd_event: {
+    args: [FFIType.uint8_t, FFIType.uint8_t, FFIType.uint32_t, FFIType.ptr],
+  },
 });
 
 function getWindow() {
@@ -56,7 +69,7 @@ function getWindow() {
 
   user32.symbols.GetWindowTextW(handle, pointer, size);
 
-  return new TextDecoder().decode(buffer);
+  return new TextDecoder("utf-16").decode(buffer);
 }
 
 function getClipboard() {
@@ -89,6 +102,10 @@ function setCursorPos(x: number, y: number) {
   }
 }
 
+function setCursorPosObj(pos) {
+  setCursorPos(pos.x, pos.y);
+}
+
 function mouseClick(button: "left" | "right") {
   let flags = 0;
   if (button === "left") {
@@ -98,10 +115,91 @@ function mouseClick(button: "left" | "right") {
     flags |= MOUSEEVENTF_RIGHTDOWN | MOUSEEVENTF_RIGHTUP;
   }
 
-  user32.symbols.mouse_event(flags, 0, 0, 0, 0)
+  user32.symbols.mouse_event(flags, 0, 0, 0, 0);
 }
 
-setCursorPos(3000, 500)
-mouseClick("right")
+async function pressControlAltC() {
+  user32.symbols.keybd_event(VK_CONTROL, 0, 0, 0);
+  user32.symbols.keybd_event(VK_MENU, 0, 0, 0);
+  user32.symbols.keybd_event(C_KEY, 0, 0, 0);
+  user32.symbols.keybd_event(C_KEY, 0, KEYEVENTF_KEYUP, 0);
+  user32.symbols.keybd_event(VK_MENU, 0, KEYEVENTF_KEYUP, 0);
+  user32.symbols.keybd_event(VK_CONTROL, 0, KEYEVENTF_KEYUP, 0);
+}
 
+async function sleep(t: number) {
+  await new Promise((r) => setTimeout(r, t));
+}
 
+const altPos = {
+  x: 2701,
+  y: 353,
+};
+
+const augPos = {
+  x: 2881,
+  y: 440,
+};
+
+const itemPos = {
+  x: 2999,
+  y: 574,
+};
+
+async function currencyClick(orb: "alt" | "aug") {
+  const pos = orb === "alt" ? altPos : augPos;
+  await sleep(500);
+  setCursorPosObj(pos);
+  await sleep(100);
+  mouseClick("right");
+  await sleep(100);
+  setCursorPosObj(itemPos);
+  await sleep(100);
+  mouseClick("left");
+}
+
+function checkTargetCondition(item: string) {
+  const regex = /explode/i;
+  return regex.test(item);
+}
+
+function checkHasPrefix(item: string) {
+  const regex = /prefix/i;
+  return regex.test(item);
+}
+
+async function getItem() {
+  await sleep(500);
+  setCursorPosObj(itemPos);
+  await sleep(500);
+  await pressControlAltC();
+  await sleep(100);
+  return getClipboard();
+}
+
+function isGameForeground() {
+  const regex = /Path of Exile/;
+  const title = getWindow();
+  const result = regex.test(title);
+  return result;
+}
+
+await sleep(2000);
+
+let item = "";
+while (isGameForeground()) {
+  item = await getItem();
+
+  if (checkTargetCondition(item)) {
+    console.log("success!")
+    break;
+  }
+
+  if (!checkHasPrefix(item)) {
+    await currencyClick("aug");
+  } else {
+    await currencyClick("alt");
+  }
+
+  await sleep(1000);
+}
